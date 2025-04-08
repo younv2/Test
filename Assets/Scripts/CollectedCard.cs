c83f89c39a0e1f231a945b79b5a74bf0a2a068fd
jaylynnmell <danmugi118@gmail.com>
2025-04-08 16:53:49 +0900

    feat : 해금 전 물음표 이미지 및 관련 로직 추가
    1) 해금 전 물음표 이미지 추가
    2) 물음표 이미지가 SetActive 되어있을 시, 카드 뒤집기 불가능한 기능 추가

--- Assets/Scripts/CollectedCard.cs
+++ Assets/Scripts/CollectedCard.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollectedCard : MonoBehaviour
@@ -7,11 +8,42 @@ public class CollectedCard : MonoBehaviour
     public Animator animator;
     public GameObject Front;
     public GameObject Back;
+    public GameObject Locked;
 
+    private bool isLocked = true;
     private bool isFlipping = false;
     private bool isBackShowing = false;
 
+    public void Update()
+    {
+        // If locked images are set active, cards cannot be clicked
+        if (Locked.activeSelf)
+        {
+            isLocked = true;
+        }
+        else if (!Locked.activeSelf)
+        {
+            isLocked = false;
+        }
+    }
+
+    // This method is called when the card is clicked
     private void OnMouseDown()
+    {
+        // If locked images are set active, cards cannot be clicked
+        if (isLocked)
+        {
+            return;
+        }
+        else if (!isLocked)
+        {
+            FlipCard();
+        }
+
+    }
+
+    // Call this method to flip the card
+    public void FlipCard()
     {
         if (isFlipping) return;
 
