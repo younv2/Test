fdf3182ef4362efa195685a2bc160265c62fe560
GonyangTest <112749400+GonyangTest@users.noreply.github.com>
2025-04-08 14:07:59 +0900

    feat : 종료 화면 패널 추가    - 성공 / 실패 텍스트 추가    - 별 스코어 별로 별 이미지 추가    - 다시 시작 / 다음 스테이지, 메인메뉴 버튼 추가

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class Star : MonoBehaviour
+{
+    public Image starImage;
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        starImage = GetComponent<Image>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Setting(int starCount)
+    {
+        if (starImage == null)
+        {
+            starImage = GetComponent<Image>();
+        }
+        if(starImage == null)
+        {
+            Debug.LogError("SpriteRenderer component is missing on the Star object!");
+            return;
+        }
+        // Debug.Log("[Star] starImage: ");
+        starImage.sprite = Resources.Load<Sprite>($"Images/Star{starCount}");
+        Debug.Log("[Star] starImage: " + starImage.sprite.name);
+    }
+}
